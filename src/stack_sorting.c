669c60a0891632766a535ae3003c9b3b52d9f5c7
Jesus Valentin Perez Garrido <jeperez-@c3r6s6.42malaga.com>
2024-10-30 11:17:51 +0100

    changed to fit the norm

--- src/stack_sorting.c
+++ src/stack_sorting.c
@@ -6,7 +6,7 @@
 /*   By: jeperez- <jeperez-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:33:03 by jeperez-          #+#    #+#             */
-/*   Updated: 2024/10/30 10:49:56 by jeperez-         ###   ########.fr       */
+/*   Updated: 2024/10/30 11:15:44 by jeperez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,42 +36,40 @@ static t_bool	is_sorted(t_list *lst)
 
 static void	sort_only_3(t_args *args, t_list **stack)
 {
+	t_list	*first_node;
+	t_list	*second_node;
+	t_list	*third_node;
+
 	if (is_sorted(*stack))
 		return ;
 	if (ft_lstsize(*stack) < 3)
 		sa(args);
 	else
 	{
-		if (*(int *)(*stack)->content < *(int *)(*stack)->next->content)
+		first_node = (int *)(*stack);
+		second_node = (int *)(*stack)->next;
+		third_node = (int *)(*stack)->next->next;
+		if (*(int *)first_node->content < *(int *)(*stack)->next->content)
 			rra(args);
-		else if (*(int *)(*stack)->content > *(int *)(*stack)->next->content
-			&& *(int *)(*stack)->content > *(int *)(*stack)->next->next->content)
+		else if (*(int *)first_node->content > *(int *)second_node->content
+			&& *(int *)first_node->content > *(int *)third_node->content)
 			ra(args);
 		if (!is_sorted(*stack))
 			sa(args);
 	}
 }
 
-/*
-1 2 3 \0
-1 3 2 rra sa
-2 3 1 rra
-3 1 2 ra
-3 2 1 ra sa
-2 1 3 sa
-*/
-
 void	sort_stack(t_args *args)
 {
 	if (is_sorted(args->stack_a))
 		return ;
 	initial_desp(args);
-	/*while (ft_lstsize(args->stack_a) > 3)
-		sort_stack(args->stack_a, args->stack_b);*/
+	while (ft_lstsize(args->stack_a) > 3)
+		push_cheapest_in(args, args->stack_a, args->stack_b);
 	sort_only_3(args, &args->stack_a);
-	/*while (ft_lstsize(args->stack_b))
-		sort_stack();
-	final_sort();*/
+	while (ft_lstsize(args->stack_b))
+		push_cheapest_out(args, args->stack_b, args->stack_a);
+	//final_sort();
 }
 
 /*
